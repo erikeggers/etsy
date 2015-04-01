a3bf0370c3f13d9126a604ef1e2282d301c088f4
Erik Eggers <em.eggers@gmail.com>
2015-04-01 17:42:46 -0400

    clean up

--- scripts/main.js
+++ scripts/main.js
@@ -68,49 +68,6 @@
         dataType: 'jsonp',
       }).done(renderData);
     });
-
-
-// $(".sort-dropdown").change(function(sort) {
-//       if ($(".sort-dropdown option:selected").text() == "Lowest Price") {
-//         legos = _.sortBy(legos, function(data) {
-//           return +lego.price;
-//           });
-//       } else if ($(".sort-dropdown option:selected").text() == "Highest Price") {
-//         legos = _.sortBy(legos, function(data) {
-//           return +lego.price;
-//           }).reverse();
-//       }
-//       changeOrder(legos);
-//     });
-
-    // function changeOrder(data) {
-    //   $output.empty();
-    //   data.forEach(function(lego) {
-    //     var legoInfo = renderTemplate('lego-item', {
-    //       image: lego.Images[0].url_170x135,
-    //       title: lego.title,
-    //       titleURL: lego.url,
-    //       shopName: lego.Shop.shop_name,
-    //       price: lego.price,
-    //       currency: lego.currency_code
-    //     });
-    //     $output.append(legoInfo);
-    //   });
-    // }
-
-    //
-    // legos.forEach(function(lego){
-    //   var legoInfo = renderTemplate('lego-item', {
-    //     image: lego.Images[0].url_170x135,
-    //     title: lego.title,
-    //     titleURL: lego.url,
-    //     shopName: lego.Shop.shop_name,
-    //     price: lego.price,
-    //     currency: lego.currency_code
-    //   });
-    //   $output.append(legoInfo);
-    // });
-
   });
 
 /*
